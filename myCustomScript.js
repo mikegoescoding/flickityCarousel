18233a3c36a4a40ca73d144ece6d83e6d3a3a505
michaeldidia <mikegoescoding@gmail.com>
2020-03-15 17:09:39 -0600

    flickity first commit

---
+++ myCustomScript.js
@@ -0,0 +1,16 @@
+$(".carousel").flickity({
+	// options
+	cellAlign: "center",
+	wrapAround: true,
+	// initialIndex: 0,
+	// arrowShape: {
+	//   x0: 10,
+	//   x1: 60,
+	//   y1: 30,
+	//   x2: 80,
+	//   y2: 40,
+	//   x3: 80
+	// },
+	cellSelector: ".carousel-cell",
+	setGallerySize: false
+});
